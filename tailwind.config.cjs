1677c9c163dc40072a6876bd2a1440040b718f90
Yongjun Park <yopark@student.42seoul.kr>
2023-02-14 22:27:27 +0900

    💄 Style: custom colors

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,6 +2,12 @@
 module.exports = {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
+    colors: {
+      spacegray: '#232627',
+      blue: '#377AFE',
+      red: '#E80D52',
+      yellow: '#EFFF14',
+    },
     extend: {},
   },
   plugins: [],
